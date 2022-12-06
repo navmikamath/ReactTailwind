0d8871331e2b291cbaae60c3dc12656eb20a3e74
navmikamath <93571278+navmikamath@users.noreply.github.com>
2022-12-06 20:01:03 +0530

    appending logo

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,6 +4,7 @@ export default function Login() {
   return (
     <div>
       <div className='absolute  bg-white  w-[1440px] h-[900px]'>
+        <img src="./images/Image 2341.png" className='absolute w-[106px] h-9 rounded-m mt-14 ml-14'/>
       <div className='bg-primary-500 mt-0 ml-[732px] w-[708px] h-[900px] rounded-none absolute'>
         <img src ="./images/Image 2342.png"  className='absolute  object-center ml-[148px] mt-[216px] w-[412px] h-[386px] rounded-none'/>
         <b className='absolute mt-[632px] ml-[218px] font-heading text-t7 text-white'>Voluptate dolor tempor</b>
